c6f0ca7a2b23795c7fb3d6923bac4e7728826365
root <root@Jonah.localdomain>
2020-11-23 23:15:15 +0100

    Funkar cleant

--- filter.js
+++ filter.js
@@ -1,13 +1,22 @@
 function filter(arr, func) {
-    
+    const secfilt = arr.lenght
+
+    for (i = 0; i < arr.lenght; i++) {
+        secfilt[i] = func(arr[i])
+    }
+    return secfilt
 }
 
-// TEST 1
+
 const numbers = [4, 10, 3, 12, 5, 16, 8, 13, 2, 9, 1, 11, 2]
-const filteredNumbers = filter(numbers, x => x < 10)
+const filteredNumbers = numbers.filter(x => x > 10)
 console.log(filteredNumbers)
 
-// TEST 2 - skriv själv
+const djur = ["Apa", "Krokodil", "Fisk", "Björn", "Säl", "giraff"]
+const shortdjur = djur.filter(djur => djur.length > 5)
+console.log(shortdjur)
 
 
-// TEST 3 - skriv själv
+const tal = [1, 2, 3, 4, 5, 6, 7, 8]
+const filttal = tal.filter(tal => tal > 5)
+console.log(filttal)
